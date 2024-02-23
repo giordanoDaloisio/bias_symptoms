5f87945a2dffe850329d64e401319cce280eee09
giordano.daloisio <giordano.daloisio@gmail.com>
2024-02-23 22:50:47 +0000

    added permutation importance analysis

--- execution_time_eval/model_training_grid_search_full/main.py
+++ execution_time_eval/model_training_grid_search_full/main.py
@@ -1,14 +1,14 @@
 import os
 import time
 
-import pyRAPL
+# import pyRAPL
 import pandas as pd
 from xgboost import XGBClassifier
 from sklearn.model_selection import GridSearchCV, KFold
 
 if __name__ == "__main__":
-    pyRAPL.setup()
-    measure = pyRAPL.Measurement("bar")
+    # pyRAPL.setup()
+    # measure = pyRAPL.Measurement("bar")
     data = pd.read_csv("bias_symptoms.csv", index_col=[0, 1])
     model = XGBClassifier()
     params = {
@@ -21,12 +21,12 @@ if __name__ == "__main__":
     os.makedirs("measures", exist_ok=True)
     os.makedirs("times", exist_ok=True)
     for i in range(20):
-        csv_output = pyRAPL.outputs.CSVOutput(
-            os.path.join("measures", f"measure_{i}.csv")
-        )
-        measure.begin()
+        # csv_output = pyRAPL.outputs.CSVOutput(
+        #     os.path.join("measures", f"measure_{i}.csv")
+        # )
+        # measure.begin()
         start_time = time.time()
-        grid = GridSearchCV(model, params, cv=5)
+        grid = GridSearchCV(model, params, cv=5, n_jobs=-1)
         grid.fit(
             data.drop(
                 columns=["statistical_parity", "equal_opportunity", "average_odds"]
@@ -34,9 +34,9 @@ if __name__ == "__main__":
             data[["statistical_parity", "equal_opportunity", "average_odds"]].values,
         )
         end_time = time.time()
-        measure.end()
-        measure.export(csv_output)
+        # measure.end()
+        # measure.export(csv_output)
         with open(os.path.join("times", f"time_{i}.txt"), "w") as f:
-            f.write(str(time) + "\n")
-        csv_output.save()
+            f.write(str(end_time - start_time) + "\n")
+        # csv_output.save()
         print(f"Round: {i} completed")
