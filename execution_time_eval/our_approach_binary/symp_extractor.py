9e01bfec0dbbcad68eb45487b2b6544261bcb94c
giordano.daloisio <giordano.daloisio@gmail.com>
2024-02-24 21:17:16 +0000

    fixed model predictions in exec time

--- execution_time_eval/our_approach_binary/symp_extractor.py
+++ execution_time_eval/our_approach_binary/symp_extractor.py
@@ -13,17 +13,14 @@ def symp_extractor(test: pd.DataFrame, true_label, positive_value, sensitive_var
     symptoms.append(
         test[[true_label, sensitive_var]].corr("kendall")[true_label][sensitive_var]
     )
-    symptoms.append(
-        test[[true_label, sensitive_var]].corr("spearman")[true_label][sensitive_var]
-    )
-    symptoms.append(metrics.group_ratio({sensitive_var: 0}))
+    symptoms.append(mutual_info_score(test[sensitive_var], test[true_label]))
     symptoms.append(metrics.compute_probs({sensitive_var: 0}, False)[0])
     symptoms.append(metrics.compute_probs({sensitive_var: 0}, False)[1])
-    symptoms.append(mutual_info_score(test[sensitive_var], test[true_label]))
-    symptoms.append(test[sensitive_var].kurt())
-    symptoms.append(test[sensitive_var].skew())
+    symptoms.append(metrics.group_ratio({sensitive_var: 0}))
     symptoms.append(1 - unpriv_prob_pos)
     symptoms.append(1 - priv_prob_pos)
     symptoms.append(unpriv_prob_pos - priv_prob_pos)
     symptoms.append((1 - unpriv_prob_pos) - (1 - priv_prob_pos))
+    symptoms.append(test[sensitive_var].kurt())
+    symptoms.append(test[sensitive_var].skew())
     return np.array(symptoms).reshape(1, -1)
