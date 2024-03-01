2b15567740a5b1caf3c932332dae902ea627a23f
giordano.daloisio <giordano.daloisio@gmail.com>
2024-03-01 18:24:17 +0000

    added results for other ml base models

--- symptoms_extraction/main.py
+++ symptoms_extraction/main.py
@@ -39,93 +39,41 @@ def analysis(
     correlation,
 ):
     symptoms = pd.DataFrame()
-    # symptoms = pd.DataFrame(
-    #     importance.importances_mean,
-    #     index=test.drop(columns=[true_label, predicted_label]).columns,
-    #     columns=["perm_importance"],
-    # )
-
     binary_variables = [
         c
         for c in test.drop(columns=[true_label, predicted_label]).columns
         if test[c].nunique() == 2
     ]
-    # symptoms = symptoms[symptoms.index.isin(binary_variables)]
-    # symptoms.reset_index(inplace=True)
-    # symptoms.rename(columns={"index": "variable"}, inplace=True)
     metrics = Metrics(test, predicted_label, true_label, positive_value)
-    # importance_coeff = model.coef_[0]
-    # importance = permutation_importance(
-    #     model,
-    #     test.drop(columns=[predicted_label, true_label], axis=1),
-    #     test[predicted_label],
-    #     scoring="accuracy",
-    # ).importances_mean
 
-    coeff = []
+    "correlation_true",
+    "mutual_info",
+    "unpriv_prob_pos",
+    "priv_prob_pos",
+    "unbalance",
+    "statistical_parity",
+    "equal_opportunity",
+    "average_odds",
+    "unpriv_prob_neg",
+    "priv_prob_neg",
+    "pos_prob",
+    "neg_prob",
+    "kurtosis_var",
+    "skew_var",
+
     sp = []
     eo = []
     aod = []
     unbalance = []
-    # sensitive_value = []
-    correlation_pred = []
     corr_true = []
-    # spearman_true = []
-    perm_importance = []
     variable = []
-    performance_score = []
     unpriv_prob = []
     priv_prob = []
     mutual_info = []
-    instance_num = []
-    log_inst_num = []
-    class_num = []
-    feat_num = []
-    log_feat_num = []
-    inst_missing_vals = []
-    perc_inst_missing_val = []
-    feat_missing_val = []
-    perc_feat_missing_val = []
-    missing_vals = []
-    perc_miss_vals = []
-    numeric_features = []
-    cat_features = []
-    ratio_num_cat = []
-    ratio_cat_num = []
-    dataset_ratio = []
-    log_dataset_ratio = []
-    inverse_ratio = []
-    log_inverse_ratio = []
-    class_prob_min = []
-    class_prob_max = []
-    class_prob_mean = []
-    class_prob_std = []
-    symbols = []
-    symbols_min = []
-    symbols_max = []
-    symbols_mean = []
-    symbols_std = []
-    symbols_sum = []
-    kurtosis_min = []
-    kurtosis_max = []
-    kurtosis_mean = []
-    kurtosis_std = []
     kurtosis_var = []
-    skew_min = []
-    skew_max = []
-    skew_mean = []
-    skew_std = []
     skew_var = []
-    class_entropy = []
     for i in binary_variables:
-        # for v in test[i].unique():
         corr_true.append(test[[true_label, i]].corr(correlation)[true_label][i])
-        # spearman_true.append(test[[true_label, i]].corr("spearman")[true_label][i])
-        # coeff.append(
-        #     importance_coeff[
-        #         test.drop(columns=[true_label, predicted_label]).columns.get_loc(i)
-        #     ]
-        # )
         unbalance.append(metrics.group_ratio({i: 0}))
 
         unpriv_prob.append(metrics.compute_probs({i: 0}, False)[0])
@@ -223,46 +171,8 @@ def analysis(
     symptoms["equal_opportunity"] = eo
     symptoms["average_odds"] = aod
     symptoms["variable"] = variable
-    symptoms["instance_num"] = instance_num
-    symptoms["log_inst_num"] = log_inst_num
-    symptoms["class_num"] = class_num
-    symptoms["feat_num"] = feat_num
-    symptoms["log_feat_num"] = log_feat_num
-    symptoms["inst_missing_vals"] = inst_missing_vals
-    symptoms["perc_inst_missing_val"] = perc_inst_missing_val
-    symptoms["feat_missing_val"] = feat_missing_val
-    symptoms["perc_feat_missing_val"] = perc_feat_missing_val
-    symptoms["missing_vals"] = missing_vals
-    symptoms["perc_miss_vals"] = perc_miss_vals
-    symptoms["numeric_features"] = numeric_features
-    symptoms["cat_features"] = cat_features
-    symptoms["ratio_num_cat"] = ratio_num_cat
-    symptoms["ratio_cat_num"] = ratio_cat_num
-    symptoms["dataset_ratio"] = dataset_ratio
-    symptoms["log_dataset_ratio"] = log_dataset_ratio
-    symptoms["inverse_ratio"] = inverse_ratio
-    symptoms["log_inverse_ratio"] = log_inverse_ratio
-    symptoms["class_prob_min"] = class_prob_min
-    symptoms["class_prob_max"] = class_prob_max
-    symptoms["class_prob_mean"] = class_prob_mean
-    symptoms["class_prob_std"] = class_prob_std
-    symptoms["symbols"] = symbols
-    symptoms["symbols_min"] = symbols_min
-    symptoms["symbols_max"] = symbols_max
-    symptoms["symbols_mean"] = symbols_mean
-    symptoms["symbols_std"] = symbols_std
-    symptoms["symbols_sum"] = symbols_sum
-    symptoms["kurtosis_min"] = kurtosis_min
-    symptoms["kurtosis_max"] = kurtosis_max
-    symptoms["kurtosis_mean"] = kurtosis_mean
-    symptoms["kurtosis_std"] = kurtosis_std
     symptoms["kurtosis_var"] = kurtosis_var
-    symptoms["skew_min"] = skew_min
-    symptoms["skew_max"] = skew_max
-    symptoms["skew_mean"] = skew_mean
-    symptoms["skew_std"] = skew_std
     symptoms["skew_var"] = skew_var
-    symptoms["class_entropy"] = class_entropy
     symptoms["unpriv_prob_neg"] = 1 - symptoms["unpriv_prob_pos"]
     symptoms["priv_prob_neg"] = 1 - symptoms["priv_prob_pos"]
     symptoms["pos_prob"] = symptoms["unpriv_prob_pos"] - symptoms["priv_prob_pos"]
