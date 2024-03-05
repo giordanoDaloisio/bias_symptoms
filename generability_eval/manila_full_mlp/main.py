4b9b1e4d9f024ed432512a1cff838444e8a79e1b
giordano.daloisio <giordano.daloisio@gmail.com>
2024-03-05 18:19:50 +0000

    added new symptoms

--- generability_eval/manila_full_mlp/main.py
+++ generability_eval/manila_full_mlp/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import time
 
-import pyRAPL
+# import pyRAPL
 from experiment import run_exp
 import warnings
 
@@ -62,29 +62,29 @@ def get_label_var(dataset):
 
 
 if __name__ == "__main__":
-    pyRAPL.setup()
-    measure = pyRAPL.Measurement("bar")
+    # pyRAPL.setup()
+    # measure = pyRAPL.Measurement("bar")
     os.makedirs("manila_results", exist_ok=True)
     os.makedirs("measures", exist_ok=True)
     os.makedirs("times", exist_ok=True)
     for i in range(20):
         for file in os.listdir("../data"):
-            csv_output = pyRAPL.outputs.CSVOutput(
-                os.path.join("measures", f"measure_{i}.csv")
-            )
+            # csv_output = pyRAPL.outputs.CSVOutput(
+            #     os.path.join("measures", f"measure_{i}.csv")
+            # )
             print(f"Starting dataset {file}")
             data = pd.read_csv(f"../data/{file}", index_col=0)
             label, pos_label, sensitive_var = get_label_var(file)
-            measure.begin()
+            # measure.begin()
             start_time = time.time()
             model, report = run_exp(data, label, sensitive_var, pos_label)
             end_time = time.time()
-            measure.end()
-            measure.export(csv_output)
+            # measure.end()
+            # measure.export(csv_output)
             # times.append(end_time - start_time)
             print(f"Dataset: {file} completed")
             report.to_csv(f"manila_results/{file}_report.csv")
         with open(os.path.join("times", f"time_{i}.txt"), "w") as f:
             f.write(str(end_time - start_time) + "\n")
-        csv_output.save()
+        # csv_output.save()
         print(f"Round: {i} completed")
--- generability_eval/merge_data.py
+++ generability_eval/merge_data.py
@@ -30,76 +30,85 @@ for file in os.listdir(base_folder):
     full_data = pd.concat([full_data, df])
 
 full_data.set_index(["variable", "data"], inplace=True)
-full_data.drop(columns=["neg_prob", "priv_prob_neg", "unpriv_prob_neg"], inplace=True)
 full_data["pos_prob"] = full_data["pos_prob"].abs()
+full_data["gini"] = (full_data["gini"] - full_data["gini"].mean()) / full_data[
+    "gini"
+].std()
+full_data["simpson"] = (full_data["simpson"] - full_data["simpson"].mean()) / full_data[
+    "simpson"
+].std()
+full_data["shannon"] = (full_data["shannon"] - full_data["shannon"].mean()) / full_data[
+    "shannon"
+].std()
+full_data["ir"] = (full_data["ir"] - full_data["ir"].mean()) / full_data["ir"].std()
 
-bias_symp = [
-    "correlation_true",
-    "mutual_info",
-    "unpriv_prob_pos",
-    "priv_prob_pos",
-    "unpriv_unbalance",
-    "priv_unbalance",
-    "statistical_parity",
-    "equal_opportunity",
-    "average_odds",
-    "pos_prob",
-    "kurtosis_var",
-    "skew_var",
-]
+# bias_symp = [
+#     "correlation_true",
+#     "mutual_info",
+#     "unpriv_prob_pos",
+#     "priv_prob_pos",
+#     "unpriv_unbalance",
+#     "priv_unbalance",
+#     "statistical_parity",
+#     "equal_opportunity",
+#     "average_odds",
+#     "pos_prob",
+#     "kurtosis_var",
+#     "skew_var",
+# ]
 
-symp_data = full_data[bias_symp]
+# symp_data = full_data[bias_symp]
 
-metafeatures = [
-    "statistical_parity",
-    "equal_opportunity",
-    "average_odds",
-    "instance_num",
-    "log_inst_num",
-    "class_num",
-    "feat_num",
-    "log_feat_num",
-    "inst_missing_vals",
-    "perc_inst_missing_val",
-    "feat_missing_val",
-    "perc_feat_missing_val",
-    "missing_vals",
-    "perc_miss_vals",
-    "numeric_features",
-    "cat_features",
-    "ratio_num_cat",
-    "ratio_cat_num",
-    "dataset_ratio",
-    "log_dataset_ratio",
-    "inverse_ratio",
-    "log_inverse_ratio",
-    "class_prob_min",
-    "class_prob_max",
-    "class_prob_mean",
-    "class_prob_std",
-    "symbols",
-    "symbols_min",
-    "symbols_max",
-    "symbols_mean",
-    "symbols_std",
-    "symbols_sum",
-    "kurtosis_min",
-    "kurtosis_max",
-    "kurtosis_mean",
-    "kurtosis_std",
-    "skew_min",
-    "skew_max",
-    "skew_mean",
-    "skew_std",
-    "class_entropy",
-]
+# metafeatures = [
+#     "statistical_parity",
+#     "equal_opportunity",
+#     "average_odds",
+#     "instance_num",
+#     "log_inst_num",
+#     "class_num",
+#     "feat_num",
+#     "log_feat_num",
+#     "inst_missing_vals",
+#     "perc_inst_missing_val",
+#     "feat_missing_val",
+#     "perc_feat_missing_val",
+#     "missing_vals",
+#     "perc_miss_vals",
+#     "numeric_features",
+#     "cat_features",
+#     "ratio_num_cat",
+#     "ratio_cat_num",
+#     "dataset_ratio",
+#     "log_dataset_ratio",
+#     "inverse_ratio",
+#     "log_inverse_ratio",
+#     "class_prob_min",
+#     "class_prob_max",
+#     "class_prob_mean",
+#     "class_prob_std",
+#     "symbols",
+#     "symbols_min",
+#     "symbols_max",
+#     "symbols_mean",
+#     "symbols_std",
+#     "symbols_sum",
+#     "kurtosis_min",
+#     "kurtosis_max",
+#     "kurtosis_mean",
+#     "kurtosis_std",
+#     "skew_min",
+#     "skew_max",
+#     "skew_mean",
+#     "skew_std",
+#     "class_entropy",
+# ]
 
-meta_data = full_data[metafeatures]
+# meta_data = full_data[metafeatures]
 
 # data_proc(full_data).to_csv(
 #     os.path.join(f"result_class_{args.folder}", "all_features.csv")
 # )
-data_proc(symp_data).to_csv(
+data_proc(full_data).to_csv(
     os.path.join(f"result_class_{args.folder}", f"bias_symptoms_{args.folder}.csv")
 )
 # data_proc(meta_data).to_csv(
